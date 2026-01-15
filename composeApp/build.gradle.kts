951669b90c43f65aa32a67dabef194f8504d4061
Mosakowski <kubamosakowski@gmail.com>
2026-01-15 16:23:52 +0100

    Fix: Disable Android target in Docker build

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -2,36 +2,55 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
+//  ZMIANA 1: Sprawdzamy, czy istnieje plik local.properties (jest lokalnie, nie ma w Dockerze)
+val localProperties = project.rootProject.file("local.properties")
+val isAndroidAvailable = localProperties.exists()
+
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.androidApplication)
+    //  ZMIANA 2: Dodajemy "apply false" - nie włączamy Androida automatycznie
+    alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
 }
 
+// 👇ZMIANA 3: Włączamy plugin Androida TYLKO jeśli mamy środowisko (lokalnie)
+if (isAndroidAvailable) {
+    apply(plugin = "com.android.application")
+}
+
 kotlin {
-    androidTarget {
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_11)
+    //  ZMIANA 4: Konfigurujemy Androida TYLKO w bloku if
+    if (isAndroidAvailable) {
+        androidTarget {
+            compilerOptions {
+                jvmTarget.set(JvmTarget.JVM_11)
+            }
         }
     }
-    
+
     js {
         browser()
         binaries.executable()
     }
-    
+
     @OptIn(ExperimentalWasmDsl::class)
     wasmJs {
         browser()
         binaries.executable()
     }
-    
+
     sourceSets {
-        androidMain.dependencies {
-            implementation(compose.preview)
-            implementation(libs.androidx.activity.compose)
+        // ZMIANA 5: Zależności Androida też wrzucamy w if (dla bezpieczeństwa)
+        if (isAndroidAvailable) {
+            val androidMain by getting {
+                dependencies {
+                    implementation(compose.preview)
+                    implementation(libs.androidx.activity.compose)
+                }
+            }
         }
+
         commonMain.dependencies {
             implementation(compose.runtime)
             implementation(compose.foundation)
@@ -51,34 +70,39 @@ kotlin {
     }
 }
 
-android {
-    namespace = "org.pracainzynierska.sportbooking"
-    compileSdk = libs.versions.android.compileSdk.get().toInt()
+//  ZMIANA 6: Cała sekcja konfiguracji Androida w klamrze if
+if (isAndroidAvailable) {
+    android {
+        namespace = "org.pracainzynierska.sportbooking"
+        compileSdk = libs.versions.android.compileSdk.get().toInt()
 
-    defaultConfig {
-        applicationId = "org.pracainzynierska.sportbooking"
-        minSdk = libs.versions.android.minSdk.get().toInt()
-        targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        defaultConfig {
+            applicationId = "org.pracainzynierska.sportbooking"
+            minSdk = libs.versions.android.minSdk.get().toInt()
+            targetSdk = libs.versions.android.targetSdk.get().toInt()
+            versionCode = 1
+            versionName = "1.0"
         }
-    }
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
+        packaging {
+            resources {
+                excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            }
         }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        buildTypes {
+            getByName("release") {
+                isMinifyEnabled = false
+            }
+        }
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_11
+            targetCompatibility = JavaVersion.VERSION_11
+        }
     }
 }
 
 dependencies {
-    debugImplementation(compose.uiTooling)
+    //  ZMIANA 7: Zależność debugowa też tylko dla Androida
+    if (isAndroidAvailable) {
+        debugImplementation(compose.uiTooling)
+    }
 }
-
